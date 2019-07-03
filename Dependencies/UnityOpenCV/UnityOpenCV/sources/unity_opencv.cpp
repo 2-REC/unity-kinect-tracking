15eece6a4d97a56480d503d52c2ffcc8f50addea
Derek Severin <severinderek@gmail.com>
2019-07-03 18:26:17 +0700

    Colour tracking test

--- Dependencies/UnityOpenCV/UnityOpenCV/sources/unity_opencv.cpp
+++ Dependencies/UnityOpenCV/UnityOpenCV/sources/unity_opencv.cpp
@@ -13,7 +13,7 @@ void ProcessImage(byte **raw, int width, int height) {
 	Mat image(height, width, CV_8UC4, *raw);
 
     // Process frame here
-    //...
+	// => Simple example: Edge detection
 	Mat edges;
 	Canny(image, edges, 50, 200);
 	cvtColor(edges, edges, COLOR_GRAY2RGBA);
@@ -26,20 +26,84 @@ void ProcessImageRegion(byte **raw, int width, int height, cv::Rect region) {
 
 	Mat image(height, width, CV_8UC4, *raw);
 
-//cout << "ROI: " << region.x << ", " << region.y << ", " << region.width << ", " << region.height << endl;
+	// Process frame here
+	// => Colour detection in ROI
+	//cout << "ROI: " << region.x << ", " << region.y << ", " << region.width << ", " << region.height << endl;
+	Mat imageROI = image(region);
 
-	Mat roi = image(region);
+	Mat imageHSV;
+	Mat threshold;
 
-    // Process frame here
-    //...
-	Mat edges;
-    Canny(roi, edges, 5, 250);
-	cvtColor(edges, edges, COLOR_GRAY2RGBA);
-    multiply(roi, edges, roi);
+	// hardcoded "light blue" colour
+	Scalar col1(106, 60, 90);
+	Scalar col2(124, 255, 255);
 
-	roi.copyTo(image(region));
-
+	cvtColor(imageROI, imageHSV, COLOR_BGR2HSV);
+	inRange(imageHSV, col1, col2, threshold);
+	cvtColor(threshold, imageROI, COLOR_GRAY2RGBA);
+	imageROI.copyTo(image(region));
 }
+
+//TODO: "GetColourBlobs(List<Colors>...)"
+/*
+TODO!
+=> From:
+https://pysource.com/2019/02/15/detecting-colors-hsv-color-space-opencv-with-python/
+
+previous based on:
+https://stackoverflow.com/questions/19189482/color-detection-in-opencv
+look at:
+- info about colours in opencv (& HSV)
+	http://www.shervinemami.info/colorConversion.html
+- pick HSV value from pixel on screen:
+    https://pastebin.com/EHz2a0YP
+
+# Red color
+	low_red = np.array([161, 155, 84])
+	high_red = np.array([179, 255, 255])
+	red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+	red = cv2.bitwise_and(frame, frame, mask=red_mask)
+
+	# Blue color
+	low_blue = np.array([94, 80, 2])
+	high_blue = np.array([126, 255, 255])
+	blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
+	blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+
+	# Green color
+	low_green = np.array([25, 52, 72])
+	high_green = np.array([102, 255, 255])
+	green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+	green = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+	# Every color except white
+	low = np.array([0, 42, 0])
+	high = np.array([179, 255, 255])
+	mask = cv2.inRange(hsv_frame, low, high)
+	result = cv2.bitwise_and(frame, frame, mask=mask)
+*/
+/*
+void ProcessImageRegion(byte **raw, int width, int height, cv::Rect region) {
+    using namespace cv;
+    using namespace std;
+
+	Mat image(height, width, CV_8UC4, *raw);
+
+	//cout << "ROI: " << region.x << ", " << region.y << ", " << region.width << ", " << region.height << endl;
+	Mat imageROI = image(region);
+
+	Mat imageHSV;
+	Mat threshold;
+
+	cvtColor(imageROI, imageHSV, COLOR_BGR2HSV);
+	inRange(imageHSV, Scalar(106, 60, 90), Scalar(124, 255, 255), threshold);
+
+	cvtColor(threshold, imageROI, COLOR_GRAY2RGBA);
+
+//TODO: do something else with found data...
+	imageROI.copyTo(image(region));
+}
+*/
 
 #ifdef __cplusplus
 }
---
+++ UnityKinectTracking/Assets/Scripts/HeadTracking.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+using Windows.Kinect;
+
+public class HeadTracking : MonoBehaviour {
+
+    const int COLOR_WIDTH = KinectInputManager.COLOR_WIDTH;
+    const int COLOR_HEIGHT = KinectInputManager.COLOR_HEIGHT;
+
+    public int roiWidthFactor = 2; // twice the head width
+    public int roiHeightFactor = 2; // twice the head height
+
+    public KinectInputManager kinectInputManager;
+    public BodyTracker2D bodyTracker;
+    public Image image;
+
+    Texture2D texture;
+
+
+    void Start() {
+        texture = new Texture2D(COLOR_WIDTH, COLOR_HEIGHT, TextureFormat.BGRA32, false);
+//        texture = new Texture2D(KinectInputManager.DEPTH_WIDTH, KinectInputManager.DEPTH_HEIGHT, TextureFormat.Alpha8, false);
+        image.material.mainTexture = texture;
+
+    }
+
+    void Update() {
+
+//TODO: do it once have found the head (only here for display purpose)
+        var rawImage = kinectInputManager.GetColorBuffer();
+
+        ulong id = bodyTracker.GetTrackedId(1);
+        if (id != 0) {
+
+            RectInt roi;
+            Vector2 position = bodyTracker.GetPosition(id, JointType.Head);
+            if (position != Vector2.zero) {
+                if ((position.x < COLOR_WIDTH) && (position.y < COLOR_HEIGHT)) {
+//TODO: determine using the depth value OR body index data (dependent on distance!)
+int roiWidth = 200 * (roiWidthFactor / 2);
+int roiHeight = 200 * (roiHeightFactor / 2);
+
+//TODO: use "min" & "max" methods directly
+                    int x = (int)position.x - roiWidth / 2;
+                    int y = (int)position.y - roiHeight / 2;
+                    int w = roiWidth;
+                    int h = roiHeight;
+
+                    if (x < 0) x = 0;
+                    if (y < 0) y = 0;
+                    if ((x + w) >= COLOR_WIDTH) w = COLOR_WIDTH - 1 - x;
+                    if ((y + h) >= COLOR_HEIGHT) h = COLOR_HEIGHT - 1 - y;
+
+                    roi = new RectInt(x, y, w, h);
+
+//                    var rawImage = kinectInputManager.GetColorBuffer();
+//...
+
+//TODO: check in library if roi is in image!!!!
+                    ProcessImageRegion(ref rawImage, COLOR_WIDTH, COLOR_HEIGHT, roi);
+                }
+            }
+        }
+
+//TODO: probably don't need that (only here for display purpose)
+        texture.LoadRawTextureData(rawImage);
+        texture.Apply();
+    }
+
+
+//    [DllImport("UnityOpenCV")]
+//    static extern void ProcessImage(ref byte[] raw, int width, int height);
+
+    [DllImport("UnityOpenCV")]
+    static extern void ProcessImageRegion(ref byte[] raw, int width, int height, RectInt roi);
+
+}
