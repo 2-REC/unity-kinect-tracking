125c122669110f6fc14ca8c462188e0e2b39660c
Derek Severin <severinderek@gmail.com>
2019-08-23 12:35:56 +0700

    Code cleaning

--- UnityKinectTracking/Assets/Scripts/HeadTracking.cs
+++ UnityKinectTracking/Assets/Scripts/HeadTracking.cs
@@ -39,7 +39,6 @@ public class HeadTracking : MonoBehaviour {
     public KinectInputManager kinectInputManager;
     public BodyTracker2D bodyTracker;
     public BodyTracker3D bodyTracker3D;
-    public Image image;
 
     public int headWidth = 20;
     public int roiWidthFactor = 2; // twice the head width
@@ -53,19 +52,26 @@ public class HeadTracking : MonoBehaviour {
     public Vector3Int topHSVMax = new Vector3Int(20, 255, 255);
     public bool filterBodyData = true;
 
-//TODO: remove...
     public Transform head;
-    public Text text;
+    bool foundOrientation;
+    Vector3 orientation;
 
+//////// DISPLAY - BEGIN
     Texture2D texture;
+    public Image image;
+    public Text text;
+//////// DISPLAY - MID
+
     int roiSize;
     Scalar[] minHSV;
     Scalar[] maxHSV;
 
 
     void Awake() {
+//////// DISPLAY - BEGIN
         texture = new Texture2D(COLOR_WIDTH, COLOR_HEIGHT, TextureFormat.BGRA32, false);
 //        texture = new Texture2D(DEPTH_WIDTH, DEPTH_HEIGHT, TextureFormat.Alpha8, false);
+//////// DISPLAY - MID
 
         minHSV = new Scalar[] {
             new Scalar(leftHSVMin),
@@ -81,15 +87,20 @@ public class HeadTracking : MonoBehaviour {
     }
 
     void Start() {
-        image.material.mainTexture = texture;
-
+        orientation = Vector3.zero;
         roiSize = (headWidth * COLOR_WIDTH) / (2 * 100);
+
+//////// DISPLAY - BEGIN
+        image.material.mainTexture = texture;
+//////// DISPLAY - MID
     }
 
     void Update() {
+        foundOrientation = false;
 
-//TODO: do it once have found the head (only here for display purpose)
+//////// DISPLAY - BEGIN
         var rawImage = kinectInputManager.GetColorBuffer();
+//////// DISPLAY - MID
 
         ulong id = bodyTracker.GetTrackedId(1);
         if (id != 0) {
@@ -97,80 +108,70 @@ public class HeadTracking : MonoBehaviour {
             if (position != Vector2.zero) {
                 if ((position.x < COLOR_WIDTH) && (position.y < COLOR_HEIGHT)) {
 
-//TODO: should check if valid bodyTracker3D as well
-//=> Or have a combined 2D+3D tracker...
+                    //TODO: should check if valid bodyTracker3D as well
+                    //=> Or have a combined 2D+3D tracker
                     Vector3 pos = bodyTracker3D.GetPosition(id, JointType.Head);
 
                     RectInt roi = GetROI(position.x, position.y, -pos.z);
 
+//////// DISPLAY - MID
 //                    var rawImage = kinectInputManager.GetColorBuffer();
+//////// DISPLAY - END
 
                     if (filterBodyData) {
                         FilterBodyData(rawImage, roi);
                     }
 
 //////// RUNTIME_COLOURS - BEGIN
-//TODO: To remove, only for test purpose (runtime colours setting in Editor)
-minHSV = new Scalar[] {
-    new Scalar(leftHSVMin),
-    new Scalar(rightHSVMin),
-    new Scalar(topHSVMin)
-};
+/*
+                    // For test purpose (runtime colours setting in Editor)
+                    minHSV = new Scalar[] {
+                        new Scalar(leftHSVMin),
+                        new Scalar(rightHSVMin),
+                        new Scalar(topHSVMin)
+                    };
 
-maxHSV = new Scalar[] {
-    new Scalar(leftHSVMax),
-    new Scalar(rightHSVMax),
-    new Scalar(topHSVMax)
-};
-//////// RUNTIME_COLOURS - END
+                    maxHSV = new Scalar[] {
+                        new Scalar(leftHSVMax),
+                        new Scalar(rightHSVMax),
+                        new Scalar(topHSVMax)
+                    };
+*/
+//////// RUNTIME_COLOURS - MID
 
 
                     Vector3[] blobs = new Vector3[3];
+                    // Blobs are defined by their position (x,y) and size (z).
+                    // If for a colour no blob has been found, its size is 0 (z=0).
                     bool detect = DetectColoursInROI(ref rawImage, COLOR_WIDTH, COLOR_HEIGHT, roi, true, 3, minHSV, maxHSV, ref blobs);
                     if (detect) {
 
                         bool haveLeft = GetPosition((int)blobs[0].x + roi.x, (int)blobs[0].y + roi.y, blobs[0].z, out Vector3 left);
-/*
-if (haveLeft) {
-    Debug.Log("LEFT POS: " + left.x + ", " + left.y + ", " + left.z);
-}
-*/
-
                         bool haveRight = GetPosition((int)blobs[1].x + roi.x, (int)blobs[1].y + roi.y, blobs[1].z, out Vector3 right);
-/*
-if (haveRight) {
-    Debug.Log("RIGHT POS: " + right.x + ", " + right.y + ", " + right.z);
-}
-*/
+                        //TODO: Extend process to 3 points
+                        //bool haveTop = GetPosition((int)blobs[2].x + roi.x, (int)blobs[2].y + roi.y, blobs[2].z, out Vector3 top);
 
-//TODO: orangish colour not reliable (too close to skin...!?)
-/*
-                        bool haveTop = GetPosition((int)blobs[2].x + roi.x, (int)blobs[2].y + roi.y, blobs[2].z, out Vector3 top);
-                        if (haveTop) {
-Debug.Log("TOP POS: " + top.x + ", " + top.y + ", " + top.z);
+                        if (haveLeft && haveRight) { // +haveTop
+                            //orientation = GetOrientation(left, right, top);
+                            orientation = GetOrientation(left, right);
+                            foundOrientation = true;
                         }
-*/
-
-//TODO: continue process
-// (handle 3rd point & compute other angles...)
-                        if (haveLeft && haveRight) {
-                            Vector3 delta = left - right;
-//Debug.Log("DELTA: " + delta.x + ", " + delta.y + ", " + delta.z);
-
-                            float angle = Vector3.SignedAngle(delta, Vector3.forward, Vector3.up) -90.0f;
-//Debug.Log("YAW: " + angle);
-                            head.eulerAngles = new Vector3(.0f, angle, .0f);
-                            text.text = "YAW: " + ((int)(angle*100)) / 100.0f;
-                        }
-
                     }
                 }
             }
         }
 
-//TODO: don't need that (only here for display purpose)
+        if (foundOrientation) {
+            head.eulerAngles = orientation;
+//////// DISPLAY - BEGIN
+            text.text = "YAW: " + ((int)(orientation.y * 100)) / 100.0f;
+//////// DISPLAY - MID
+        }
+
+//////// DISPLAY - BEGIN
         texture.LoadRawTextureData(rawImage);
         texture.Apply();
+//////// DISPLAY - MID
     }
 
 
@@ -233,28 +234,43 @@ Debug.Log("TOP POS: " + top.x + ", " + top.y + ", " + top.z);
         }
     }
 
+    //TODO: Add more checks
     bool GetPosition(int x, int y, float size, out Vector3 position) {
         position = Vector3.zero;
 
-//Debug.Log("IN: " + x + ", " + y + ", size: " + size);
+        //Debug.Log("IN: " + x + ", " + y + ", size: " + size);
+
         if (size == 0.0) {
             return false;
         }
 
-//TODO!
+        //TODO!
         // check position & size of blobs
         //...
-// - size "close" to 1/4 head size (?) (& >0)
-// (size is diameter in pixels)
-// ?- X & Y in ROI
-// - depth "close" to Head's Z
-//Debug.Log("HEAD Z: " + -pos.z);
+        // - size "close" to 1/4 head size (?) (& >0)
+        // (size is diameter in pixels)
+        // ?- X & Y in ROI
+        // - depth "close" to Head's Z
+        //Debug.Log("HEAD Z: " + -pos.z);
 
 
         position = kinectInputManager.GetWorldPositionFromColor(x, y);
-//Debug.Log("OUT: " + position);
+        //Debug.Log("OUT: " + position);
 
         return true;
+    }
+
+    //TODO: Extend process to 3 points
+    // => Handle 3rd point & compute other angles
+    // BUT: Orange colour not reliable (too close to skin...!?)
+    Vector3 GetOrientation(Vector3 left, Vector3 right) {
+        Vector3 delta = left - right;
+        //Debug.Log("DELTA: " + delta.x + ", " + delta.y + ", " + delta.z);
+
+        float angle = Vector3.SignedAngle(delta, Vector3.forward, Vector3.up) - 90.0f;
+        //Debug.Log("YAW: " + angle);
+
+        return new Vector3(.0f, angle, .0f);
     }
 
 
